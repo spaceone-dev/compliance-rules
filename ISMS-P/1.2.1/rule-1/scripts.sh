66aceeecf4fcf92a5c962cf2040aa437fa4156a1
Choonho Son <choonhoson@megazone.com>
2020-08-05 16:37:17 +0900

    Update sample
    Signed-off-by: Choonho Son <choonhoson@megazone.com>

---
+++ ISMS-P/1.2.1/rule-1/scripts.sh
@@ -0,0 +1,11 @@
+# SpaceONE ctl shell
+
+# List Servers
+spacectl list inventory.Server -c server_id, name, project_id
+
+# Member of Project
+spacectl list_members identity.Project -c project_id, user_id
+
+
+
+# 
